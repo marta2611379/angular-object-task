d494e5ed7545af9a55f89268c91765dc1565c27b
marta2611379 <54032814+marta2611379@users.noreply.github.com>
2019-10-11 12:09:29 +0300

    commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: '<h1>New tag from ts file</h1>',
   styleUrls: ['./app.component.css']
-  // styles: [`h1{
-  //   color: red;
-  // }`],
-  // providers: []
 })
 export class AppComponent {
   title = 'First Angular project';
